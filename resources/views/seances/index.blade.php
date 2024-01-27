db87df0a6bbde69f4e9ea7c9d376779b9a7686f1
Heremoana <developpeur@bosspacific.com>
2024-01-26 18:16:40 -1000

    modif: space

--- resources/views/seances/index.blade.php
+++ resources/views/seances/index.blade.php
@@ -79,17 +79,13 @@
                                             Annuler
                                         </a>
                                     </td>
-
                                     @else
-
                                     <td class="px-6 py-4 text-sm leading-5 text-white-900 whitespace-no-wrap text-center">
                                         <a class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-700 shadow-sm transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25">
                                             Complet
                                         </a>
                                     </td>
                                     @endif
-
-
 
                                     @if($is_Coach)
                                     <td class="px-6 py-4 text-sm leading-5 text-white-900 whitespace-no-wrap text-center">
